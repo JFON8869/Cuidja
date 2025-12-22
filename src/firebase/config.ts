a44b242fe08bad8387315297333e7bd3b097dbf6
JOSÉ FÉLIX <felixestudosatuais@gmail.com>
2025-12-22 14:31:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2675297869-48679",
+  "appId": "1:895963079338:web:bf6d08533782f3137e66d3",
+  "apiKey": "AIzaSyAzKG0Cb5-q9pUqjVf3PjnqT3esZgBaUrE",
+  "authDomain": "studio-2675297869-48679.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "895963079338"
+};
