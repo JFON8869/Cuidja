f00c1da141fffb063015ee7a4997b9d8e44b74b9
JOSÉ FÉLIX <felixestudosatuais@gmail.com>
2025-12-26 15:02:02 +0000

    utilize esse icone para restaurantes

--- src/components/icons/categories/RestaurantIcon.tsx
+++ src/components/icons/categories/RestaurantIcon.tsx
@@ -8,27 +8,35 @@ export const RestaurantIcon = ({ className }: { className?: string }) => (
     xmlns="http://www.w3.org/2000/svg"
     preserveAspectRatio="xMidYMid meet"
   >
-    <g strokeWidth="5" strokeLinecap="round" strokeLinejoin="round">
-      {/* Plate */}
-      <circle cx="50" cy="50" r="28" fill="#78C2A5" stroke="none" />
-      <circle cx="50" cy="50" r="16" fill="#E86A5D" stroke="none" />
-      
-      {/* Plate Arcs */}
-      <path d="M28 25 A 35 35 0 0 1 72 25" fill="none" stroke="#F4B164" />
-      <path d="M28 75 A 35 35 0 0 0 72 75" fill="none" stroke="#F4B164" />
-
+    <g strokeLinecap="round" strokeLinejoin="round" strokeWidth="4.5" stroke="#3D4144">
       {/* Fork */}
-      <g stroke="#3D4144">
-        <path d="M15 80 C 15 70, 25 65, 25 50 V 20" fill="none" />
-        <path d="M15 80 L 15 90 A 5 5 0 0 0 20 95 H 30 A 5 5 0 0 0 35 90 L 35 80" fill="#F4B164" stroke="none" />
-        <path d_fork_tines="M 15 28 C 15 20, 20 20, 20 20 M 35 28 C 35 20, 30 20, 30 20 M 25 20 V 35" fill="none" />
-      </g>
-      
+      <path
+        d="M26 80V45 M26 35 C 26 25, 18 25, 18 35 M26 35 C 26 25, 34 25, 34 35 M26 22 V 35"
+        fill="none"
+      />
+      <path
+        d="M21 80 H 31 V 50 C 31 45, 21 45, 21 50 V 80 Z"
+        fill="#F4B164"
+      />
+
       {/* Knife */}
-      <g stroke="#3D4144">
-        <path d="M75 80 L 75 90 A 5 5 0 0 1 70 95 H 80 A 5 5 0 0 1 85 90 L 85 80" fill="#F4B164" stroke="none"/>
-        <path d="M75 80 V 25 C 75 15, 90 15, 90 25 L 75 45 Z" fill="#3D4144" stroke="none" />
-        <path d="M78 55 L 78 65" stroke="white" strokeWidth="4" />
+      <path
+        d="M74 80 V 35 C 74 25, 82 25, 82 35 L 74 80 Z"
+        fill="#F4B164"
+      />
+       <path
+        d="M74 45 L 82 45"
+        stroke="white"
+        strokeWidth="3.5"
+        strokeLinecap="butt"
+      />
+
+      {/* Plate and Arcs */}
+      <g stroke="none">
+        <path d="M 50 18 A 28 28 0 0 0 50 82" fill="none" stroke="#F4B164" strokeWidth="5"/>
+        <circle cx="50" cy="50" r="22" fill="#78C2A5" />
+        <circle cx="50" cy="50" r="15" fill="#FFFFFF" />
+        <circle cx="50" cy="50" r="12" fill="#E86A5D" />
       </g>
     </g>
   </svg>
