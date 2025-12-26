569a312ba14761b90ae898d11860418500874181
JOSÉ FÉLIX <felixestudosatuais@gmail.com>
2025-12-26 15:00:26 +0000

    use esse sgv como o icone de restaurante  certifique-se de retirar o fun

--- src/components/icons/categories/RestaurantIcon.tsx
+++ src/components/icons/categories/RestaurantIcon.tsx
@@ -8,17 +8,28 @@ export const RestaurantIcon = ({ className }: { className?: string }) => (
     xmlns="http://www.w3.org/2000/svg"
     preserveAspectRatio="xMidYMid meet"
   >
-    <g>
-      <path
-        d="M 80 45 A 20 20 0 0 0 50 25 A 20 20 0 0 0 20 45 L 80 45"
-        fill="#D29A67"
-      />
-      <rect x="20" y="45" width="60" height="10" fill="#E84D44" />
-      <path
-        d="M 20 55 C 20 60 25 65 30 65 L 70 65 C 75 65 80 60 80 55 L 20 55"
-        fill="#90C35B"
-      />
-      <rect x="20" y="65" width="60" height="10" rx="5" fill="#D29A67" />
+    <g strokeWidth="5" strokeLinecap="round" strokeLinejoin="round">
+      {/* Plate */}
+      <circle cx="50" cy="50" r="28" fill="#78C2A5" stroke="none" />
+      <circle cx="50" cy="50" r="16" fill="#E86A5D" stroke="none" />
+      
+      {/* Plate Arcs */}
+      <path d="M28 25 A 35 35 0 0 1 72 25" fill="none" stroke="#F4B164" />
+      <path d="M28 75 A 35 35 0 0 0 72 75" fill="none" stroke="#F4B164" />
+
+      {/* Fork */}
+      <g stroke="#3D4144">
+        <path d="M15 80 C 15 70, 25 65, 25 50 V 20" fill="none" />
+        <path d="M15 80 L 15 90 A 5 5 0 0 0 20 95 H 30 A 5 5 0 0 0 35 90 L 35 80" fill="#F4B164" stroke="none" />
+        <path d_fork_tines="M 15 28 C 15 20, 20 20, 20 20 M 35 28 C 35 20, 30 20, 30 20 M 25 20 V 35" fill="none" />
+      </g>
+      
+      {/* Knife */}
+      <g stroke="#3D4144">
+        <path d="M75 80 L 75 90 A 5 5 0 0 1 70 95 H 80 A 5 5 0 0 1 85 90 L 85 80" fill="#F4B164" stroke="none"/>
+        <path d="M75 80 V 25 C 75 15, 90 15, 90 25 L 75 45 Z" fill="#3D4144" stroke="none" />
+        <path d="M78 55 L 78 65" stroke="white" strokeWidth="4" />
+      </g>
     </g>
   </svg>
 );
