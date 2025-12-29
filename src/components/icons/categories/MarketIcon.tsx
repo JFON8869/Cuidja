5ef1ec1d3dc636b4f11d32850d1298a916cbbf25
JOSÉ FÉLIX <felixestudosatuais@gmail.com>
2025-12-29 22:20:32 +0000

    os ícones de categorias estão destorcidos crie ícones simples

--- src/components/icons/categories/MarketIcon.tsx
+++ src/components/icons/categories/MarketIcon.tsx
@@ -8,8 +8,6 @@ export const MarketIcon = ({ className }: { className?: string }) => (
     className={cn('h-10 w-10', className)}
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      d="M12,3L2,12V21H22V12L12,3M12,7.7L18,12.5V19H6V12.5L12,7.7M12,13C10.9,13 10,12.1 10,11C10,9.9 10.9,9 12,9C13.1,9 14,9.9 14,11C14,12.1 13.1,13 12,13Z"
-    />
+    <path d="M12,3L2,12V21H22V12L12,3M19,19H5V13.5L12,7.2L19,13.5V19M12,17A2,2 0 0,1 10,15A2,2 0 0,1 12,13A2,2 0 0,1 14,15A2,2 0 0,1 12,17Z" />
   </svg>
 );
--- src/components/icons/categories/RestaurantIcon.tsx
+++ src/components/icons/categories/RestaurantIcon.tsx
@@ -8,8 +8,6 @@ export const RestaurantIcon = ({ className }: { className?: string }) => (
     className={cn('h-10 w-10', className)}
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      d="M16,5V11H21V5M21,3A2,2 0 0,1 23,5V11A2,2 0 0,1 21,13H16V21H14V3H21M11,8V10H9V8M11,5V7H9V5M7,8V10H5V8M7,5V7H5V5M3,8V10H1V8M3,5V7H1V5M11,3H9V1H11V3M7,3H5V1H7V3M3,3H1V1H3V3Z"
-    />
+    <path d="M11,2V22C5.9,21.5 2,17.1 2,12C2,6.9 5.9,2.5 11,2M13,2C18.1,2.5 22,6.9 22,12C22,17.1 18.1,21.5 13,22V2Z" />
   </svg>
 );
