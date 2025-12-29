aa76072ab191a2ec2aca29b41058805dbcde4245
JOSÉ FÉLIX <felixestudosatuais@gmail.com>
2025-12-29 23:38:30 +0000

    Falha no upload da imagem. Tente novamente. voltamos ao erro, em ambient

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2675297869-48679",
   "appId": "1:895963079338:web:bf6d08533782f3137e66d3",
-  "storageBucket": "studio-2675297869-48679.firebasestorage.app",
+  "storageBucket": "studio-2675297869-48679.appspot.com",
   "apiKey": "AIzaSyAzKG0Cb5-q9pUqjVf3PjnqT3esZgBaUrE",
   "authDomain": "studio-2675297869-48679.firebaseapp.com",
   "measurementId": "",
