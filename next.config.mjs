118a18f800f2d817ccc26a5764b5172891d1d264
JOSÉ FÉLIX <felixestudosatuais@gmail.com>
2026-01-02 22:06:28 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.mjs
@@ -0,0 +1,56 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {
+    // A propriedade `allowedDevOrigins` foi movida para fora deste objeto.
+  },
+  webpack(config, { isServer, dev }) {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      }
+    }
+    return config
+  },
+  // Adicionado para permitir requisições do ambiente de desenvolvimento do Studio
+  allowedDevOrigins: ["*.cloudworkstations.dev"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https' ,
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
