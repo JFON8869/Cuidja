2e1477189bf6c4d8528e5a7fbd1be71eb00343cb
JOSÉ FÉLIX <felixestudosatuais@gmail.com>
2026-01-02 22:08:17 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   experimental: {
     // A propriedade `allowedDevOrigins` foi movida para fora deste objeto.
@@ -53,4 +52,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
